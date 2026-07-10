96eb7fb7ae9898d6b049c55153d83f780dc045f8
CodeWithRaviX <ravikishany254@gmail.com>
2026-07-10 16:57:53 +0530

    Update axios base URL to include '/api'

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://avento-b4dk.onrender.com',
+    baseURL: 'https://avento-b4dk.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
